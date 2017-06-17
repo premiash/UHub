dea21e7b7a93861763f912f85593a7a750b4bf3e
Ashmy Selvamony <premiash@gmail.com>
2017-06-17 18:31:34 -0400

    Integrated \theme assets into React modules (1st cut changes)

--- web-app/app/app.js
+++ web-app/app/app.js
@@ -4,6 +4,8 @@ var ReactDOM = require("react-dom");
 
 // Include the main Main Component
 var Main = require("./components/Main");
+var Login = require("./components/pages/login/Login");
 
 // This code here allows us to render our main component (in this case Main)
-ReactDOM.render(<Main />, document.getElementById("app"));
+//ReactDOM.render(<Main />, document.getElementById("app"));
+ReactDOM.render(<Login />, document.getElementById("app"));
